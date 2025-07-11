f73cd6b1b7324eec2fb90d802060249419b21f9a
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-07-11 11:31:54 +0100

    add config class of websocket

--- src/main/java/com/hicham/chat/config/WebSocketConfig.java
+++ src/main/java/com/hicham/chat/config/WebSocketConfig.java
@@ -8,6 +8,15 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
 
 }
