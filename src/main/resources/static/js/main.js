886ab391df958befe7e6635efec5d38c683c865d
Hicham-Azeroual <hichamazroual2002@gmail.com>
2025-07-11 12:32:02 +0100

    add the front basic implemntation

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,76 @@
+'use strict';
+var usernamePage=document.querySelector('#username-page');
+var chatPage=document.querySelector('#chat-page');
+var usernameForm=document.querySelector('#usernameForm');
+var messageForm=document.querySelector('#messageForm');
+var messageInput=document.querySelector('#message');
+var messageArea=document.querySelector('#messageArea');
+var connectingElement=document.querySelector('.connecting');
+
+var stompClient=null;
+var username=null;
+
+var colors=[
+    '#2196F3',
+    '#32c787',
+    '#00BCD4',
+    '#ff565a',
+    '#ffc107',
+    '#ff85af',
+    '#FF9800',
+    '#39bbb0'
+];
+
+usernamePage.addEventListener('submit',connect,false);
+
+function sendMessage(event) {
+    var messageContent=messageInput.value.trim();
+    if(messageContent && stompClient) {
+        var chatMessage={
+            sender:username,
+            content:messageInput.value,
+            type:'CHAT'
+        };
+        stompClient.send("/app/chat.sendMessage",{},JSON.stringify(chatMessage));
+        messageInput.value='';
+    }
+    event.preventDefault();
+
+}
+
+messageForm.addEventListener('submit',sendMessage,false);
+
+function onMessageReceived() {
+
+}
+
+function onConnected(options) {
+
+    stompClient.subscribe('/topic/public', onMessageReceived);
+
+    stompClient.send("/app/chat.addUser",{},JSON.stringify({'sender':username,'type':'JOIN'}));
+    connectingElement.classList.add('hidden');
+
+}
+
+function onError() {
+connectingElement.textContent='Could not connect to WebSocket server. Please refresh this page to try again!';
+connectingElement.style.color='red';
+
+}
+
+function connect(event) {
+    username=document.querySelector('#name').value.trim();
+
+    if(username) {
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        var socket=new SockJS('/ws');
+        stompClient=Stomp.over(socket);
+
+        stompClient.connect({},onConnected,onError);
+    }
+
+    event.preventDefault();
+}
